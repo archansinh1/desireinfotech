3884519367c0e05fc334f1e910a8caf2dc4d3c98
archansinh1 <142336231+archansinh1@users.noreply.github.com>
2023-08-16 10:59:51 +0530

    Redux actionfile

---
+++ index.jsx
@@ -0,0 +1,25 @@
+// PAGE 1
+export const inc = () =>
+{
+    return{
+        type: "INC"
+    }
+}
+export const dic = () =>
+{
+    return{
+        type: "DIC"
+    }
+}
+export const mul = () =>
+{
+    return{
+        type: "MUL"
+    }
+}
+export const div = () =>
+{
+    return{
+        type: "DIV"
+    }
+}
